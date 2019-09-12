71e8f2f3968b70a9ec0b867a4c88434726257d6b
Taylor Lacerte <taylormlacerte@gmail.com>
2019-09-12 12:34:02 -0500

    migrated first event model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
 
-# Create your models here.
+CATEGORIES = ('Concert', 'Networking', 'Food/Drink', 'Market')
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField('event date')
+    category = models.CharField(
+        max_length=30,
+        choices=CATEGORIES
+    ),
+    location = models.CharField(max_length=100)
+
