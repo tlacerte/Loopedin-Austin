64b8071670d92c90e628fa099cc3eceae187d6f7
Taylor Lacerte <taylormlacerte@gmail.com>
2019-09-13 12:01:02 -0500

    added url

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('accounts/signup', views.signup, name='signup'),
+  path('events/', views.EventsView.as_view(), name='events_list')
 ]
