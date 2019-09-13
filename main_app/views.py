6d5832d1472bd304de932d6d4d47d9aacf46d208
Taylor Lacerte <taylormlacerte@gmail.com>
2019-09-13 10:25:56 -0500

    Heather and Taylor - forms module to override signup form

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 
 def home(request):
   return render(request, 'index.html')
@@ -9,18 +9,13 @@ def home(request):
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
-    form = UserCreationForm(request.POST)
+    form = SignUpForm(request.POST)
     if form.is_valid():
-      # This will add the user to the database
       user = form.save()
-      # This is how we log a user in via code
       login(request, user)
       return redirect('index')
     else:
       error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
-  form = UserCreationForm()
+  form = SignUpForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
