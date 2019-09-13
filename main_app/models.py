d909718908fd732e6a04aabc77a7591d81d39de9
Taylor Lacerte <taylormlacerte@gmail.com>
2019-09-13 10:20:46 -0500

    Heather and Taylor - basic auth complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
+from django.contrib.auth.models import User
 
 CATEGORIES = ('Concert', 'Networking', 'Food/Drink', 'Market')
 
@@ -12,6 +13,7 @@ class Event(models.Model):
         choices=CATEGORIES
     ),
     location = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
