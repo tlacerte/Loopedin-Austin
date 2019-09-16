9831aa14a9e1cfeeed8699464308eb463d8f149f
Heather Etheridge <heathernetheridge@gmail.com>
2019-09-16 12:05:41 -0500

    testing new code

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
   path('', views.home, name='index'),
   path('accounts/signup', views.signup, name='signup'),
   path('events/list', EventsList.as_view(), name='events_list'),
-  path('events/create', views.show_event_create, name='show_event_create'),
-  path('events/create/new', views.event_create, name='event_create'),
+  # path('events/create', views.show_event_create, name='show_event_create'),
+  # path('events/create/new', views.event_create, name='event_create'),
+  #NEW PATH BELOW
+  path('events/create/', views.EventCreate.as_view(), name='event_create'),
   path('events/<int:event_id>', views.event_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -33,14 +33,23 @@ def show_event_create(request):
   user = User.objects.get(id=request.user.id)
   return render(request, 'events/create.html')
 
-def event_create(request):
-  event_form = CreateEventForm(request.POST)
-  if event_form.is_valid():
-    new_event = event_form.save(commit=False)
-    new_event.user_id = request.user.id
-    new_event.save()
-  return redirect('events_list')
+# def event_create(request):
+#   event_form = CreateEventForm(request.POST)
+#   if event_form.is_valid():
+#     new_event = event_form.save(commit=False)
+#     new_event.user_id = request.user.id
+#     new_event.save()
+#   return redirect('events_list')
+class EventCreate(LoginRequiredMixin, CreateView):
+  model = Event
+  fields = ['name', 'date', 'category', 'location']
+  def form_valid(self, form):
+  # Assign the logged in user (self.request.user)
+    form.instance.user = self.request.user
+  # Let the CreateView do its job as usual
+    return super().form_valid(form)  
 
+    
 def event_detail(request, event_id):
   event = Event.objects.get(id=event_id)
   return render(request, 'events/detail.html')
