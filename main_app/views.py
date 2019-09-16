b210d9c23bdee6b16fe506ae471cd308dfe20632
Taylor Lacerte <taylormlacerte@gmail.com>
2019-09-16 11:57:29 -0500

    details page renders event

--- main_app/views.py
+++ main_app/views.py
@@ -43,6 +43,4 @@ def event_create(request):
 
 def event_detail(request, event_id):
   event = Event.objects.get(id=event_id)
-  return render(request, 'events/detail.html')
-
-  
+  return render(request, 'events/detail.html', { 'event': event })
